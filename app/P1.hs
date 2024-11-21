b9eb55bd63d2e9b7291e676e51c387f8d7042cdb
valfranciscangeli <vvfranciscangeli@gmail.com>
2024-11-20 23:16:03 -0300

    p2 lista

--- app/P1.hs
+++ app/P1.hs
@@ -32,21 +32,9 @@ seqProd p1 p2 = mkPoly (map (coeffProd p1 p2) [0..d])  where
 
 -- parte (b)
 -- se ejecuta con: cabal run p1-exe +RTS -s
--- al ejecutar en diferentes ocasiones los resultados eran muy variados así que en adelante los resultados son 
--- al tomar un promedio de 10 ejecuciones por cada funcion
-  {-
-  Tiempo elapsed por ejecucion:
-  1. 0.900s
-  2. 0.200s
-  3. 0.190s
-  4. 0.190s
-  5. 0.180s
-  6. 0.200s
-  7. 0.190s
-  8. 0.210s
-  9. 0.190s
-  10. 0.190s
-  -}
+-- al ejecutar en diferentes ocasiones los resultados eran muy variados asi que 
+-- se toma como resultado el promedio de 10 ejecuciones
+
 -- Elapsed time: 0.264s
 
 -- parte (c)
@@ -62,19 +50,8 @@ parProd p1 p2 = mkPoly (concat coeffs)
 
 -- parte (d)
 -- se ejecuta con: cabal run p1-exe +RTS -N2 -s
-{-
-  Tiempo elapsed por ejecucion:
-  1. 0.180s
-  2. 0.190s
-  3. 0.190s
-  4. 0.190s
-  5. 0.180s
-  6. 0.180s
-  7. 0.190s
-  8. 0.190s
-  9. 0.190s
-  10. 0.170s
-  -}
+-- se toma como resultado el promedio de 10 ejecuciones
+
 -- Elapsed time: 0.185s
 
 
@@ -96,36 +73,25 @@ par1Prod p1 p2 = mkPoly coeffs
 
 -- parte (g)
 -- se ejecuta con: cabal run p1-exe +RTS -N2 -s
-{-
-  Tiempo elapsed por ejecucion:
-  1. 0.170s
-  2. 0.200s
-  3. 0.180s
-  4. 0.190s
-  5. 0.180s
-  6. 0.190s
-  7. 0.190s
-  8. 0.190s
-  9. 0.190s
-  10. 0.190s
-  -}
+-- se toma como resultado el promedio de 10 ejecuciones
+
 -- Elapsed time: 0.187s
 -- Speedup: 1,411 
 
 -- parte (h)
 {-
-Al comparar el calculo secuencial con la primera version paralela se tiene un speedup mucho menor al esperado en teoría, que sería el doble. En este caso, esto se puede deber a que se tienen otros procesos necesarios que no pueden ser paralelizados, como es el dividir la lista de indices en sublistas de 15 elementos y el concatenar los resultados para cada sublista para obtener el resultado final. Estos procesos suman un costo fijo que no estaba en la implementación secuencial original. A esto, se suma la creación de los trabajos o sparks para calcular los coeficientes para cada sublistas, que tienen un costo en si mismos. 
-Lo mismo ocurre con el speedup de la segunda versión paralela, donde hay casi nula diferencia con la primera versión- Esto se explica principalmente por la sobrecarga del codigo con la creacion de sparks para cada uno de los elementos en la lista de indices, manteniendo el uso de solo 2 cores, provocando que las tareas deban ir quedando "en cola" y no se resuelvan todas de forma paralela.
+Al comparar el calculo secuencial con la primera version paralela se tiene un speedup mucho menor al esperado en teoria, que seria el doble. En este caso, esto se puede deber a que se tienen otros procesos necesarios que no pueden ser paralelizados, como es el dividir la lista de indices en sublistas de 15 elementos y el concatenar los resultados para cada sublista para obtener el resultado final. Estos procesos suman un costo fijo que no estaba en la implementacion secuencial original. A esto, se suma la creacion de los trabajos o sparks para calcular los coeficientes para cada sublistas, que tienen un costo en si mismos. 
+Lo mismo ocurre con el speedup de la segunda version paralela, donde hay casi nula diferencia con la primera version- Esto se explica principalmente por la sobrecarga del codigo con la creacion de sparks para cada uno de los elementos en la lista de indices, manteniendo el uso de solo 2 cores, provocando que las tareas deban ir quedando "en cola" y no se resuelvan todas de forma paralela.
 -}
 
 -- parte (i)
 {-
-Esto se realiza porque el producto de polinimios genera una lista con 4900 coeficientes, lo cual ocupa una gran cantidad de espacio en la consola y añade tiempo de ejecución que podría afectar los resultados. Sin embargo, no es necesario aplicarla si el fin esforzar la evaluación del calculo del producto en general, ya que, como se necesita mostrar el resultado en pantalla, el calculo es forzado por la funcipon print. 
-En el caso de las funciones  creadas para el calculo paralelo, se fuerza el calculo en el mismo retorno, ya que debía retornar para poder usar la sintaxis do, pero si no fuera este caso, el print también forzaría el calculo del producto. Es diferente al caso visto en clases, ya que la evaluación lazy provocaría que en el ejemplo donde se imprime el largo de la lista resultante, esta no se evalúe, ya que no es necesario conocer el contenido de la lista para calcular su largo. 
+Esto se realiza porque el producto de polinimios genera una lista con 4900 coeficientes, lo cual ocupa una gran cantidad de espacio en la consola y añade tiempo de ejecucion que podria afectar los resultados. Sin embargo, no es necesario aplicarla si el fin esforzar la evaluacion del calculo del producto en general, ya que, como se necesita mostrar el resultado en pantalla, el calculo es forzado por la funcipon print. 
+En el caso de las funciones  creadas para el calculo paralelo, se fuerza el calculo en el mismo retorno, ya que debia retornar para poder usar la sintaxis do, pero si no fuera este caso, el print tambien forzaria el calculo del producto. Es diferente al caso visto en clases, ya que la evaluacion lazy provocaria que en el ejemplo donde se imprime el largo de la lista resultante, esta no se evalue, ya que no es necesario conocer el contenido de la lista para calcular su largo. 
 -}
 
 
--- Determina el número de coeficientes no nulos de un polinomio
+-- Determina el numero de coeficientes no nulos de un polinomio
 nonNullCoeff :: Poly -> Int
 nonNullCoeff = foldr (\c rec -> if c == 0 then rec else rec + 1) 0
 
--- app/P2.hs
+++ app/P2.hs
@@ -1,29 +1,52 @@
-module P2 where
+module Main where
 
+import Control.Parallel.Strategies (parList, rpar, runEval)
+import Data.List.Split (chunksOf)
 
 type Number = Double
 
 -- parte (a)
 integral :: (Number -> Number) -> Number -> Number -> Int -> Number
-integral = undefined -- complete aquí su código
-
+integral f a b n = h2 * sumatoria
+  where
+    h = (b - a) / fromIntegral n
+    h2 = h / 2
+    sumatoria = sum [f (a + fromIntegral i * h) + f (a + (fromIntegral i + 1) * h) | i <- [0 .. n]]
 
 -- parte (b)
-pintegral :: (Number -> Number) -> Number -> Number -> Int -> Number
-pintegral = undefined -- complete aquí su código
+integralaux :: (Number -> Number) -> Number -> Number -> [Int] -> Number
+integralaux f a h ies = h2 * sumatoria
+  where
+    h2 = h / 2
+    sumatoria = sum [f (a + fromIntegral i * h) + f (a + (fromIntegral i + 1) * h) | i <- ies]
 
+pintegral :: (Number -> Number) -> Number -> Number -> Int -> Number
+pintegral f a b n = sum resultado
+  where
+    indices = [0 .. n]
+    h = (b - a) / fromIntegral n
+    groupedIndices = chunksOf 50 indices
+    resultado = runEval $ parList rpar $ map (integralaux f a h) groupedIndices
 
 -- parte (c)
--- Elapsed time sequential version: complete aquí
--- Elapsed time parallel version: complete aquí
--- Speedup: complete aquí
+-- se ejecuta con: cabal run p2-exe +RTS -N2 -s
+-- se toma como resultado el promedio de 10 ejecuciones
 
+-- Elapsed time sequential version: 1,080s
+-- Elapsed time parallel version: 1,094s
+-- Speedup: 0,987
 
-main :: IO()
+main :: IO ()
 main = do
-  let f = \x -> 2*x^2 + 3*x^10 - x^6 + 10*x^30 - 8*x^25
+  let f = \x -> 2 * x ^ 2 + 3 * x ^ 10 - x ^ 6 + 10 * x ^ 30 - 8 * x ^ 25
       a = 0
       b = 100
       n = 20000
+
+  -- secuencial ==========
+  --print (integral f a b n)    
+
+  -- paralelo ============
   print (pintegral f a b n)
+
   return ()
